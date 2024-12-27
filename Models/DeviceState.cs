15e1ca19764b4998a083285de229de9a3ae84a24
Jari Suutari <jari.suutari@gmail.com>
2024-12-27 21:14:53 +0200

    Added DeviceState -model

---
+++ Models/DeviceState.cs
@@ -0,0 +1,10 @@
+﻿namespace RIoT2.Core.Models
+{
+    public class DeviceState
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public DeviceState State { get; set; }
+    }
+}
