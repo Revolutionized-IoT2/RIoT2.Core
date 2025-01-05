a8ef07fe7ce8843e1651d30a20619f64e4b366d0
Jari Suutari <jari.suutari@gmail.com>
2025-01-05 18:55:40 +0200

    Added Url to NodeConfiguration

--- Models/NodeConfiguration.cs
+++ Models/NodeConfiguration.cs
@@ -6,6 +6,7 @@
         public string MqttServerUrl { get; set; }
         public string MqttUsername { get; set; }
         public string MqttPassword { get; set; }
+        public string Url { get; set; }
         public string GetTopic(MqttTopic topic) 
         {
             return Constants.Get(Id, topic);
