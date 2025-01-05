4bab59b1aab70e7990752eebbc98f9bc1a7f10ff
Jari Suutari <jari.suutari@gmail.com>
2025-01-05 17:58:26 +0200

    Added Initialize to IDevicePlugin

--- Interfaces/IDevicePlugin.cs
+++ Interfaces/IDevicePlugin.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 
 namespace RIoT2.Core.Interfaces
 {
     public interface IDevicePlugin
     {
+        void Initialize(IServiceCollection services);
         List<IDevice> Devices { get; }
     }
 }
